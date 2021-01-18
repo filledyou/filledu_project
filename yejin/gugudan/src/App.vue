e2b19d4fa9502ac3f4688c8afebc91bc2c2dc9ca
JJine <51090557+JJine@users.noreply.github.com>
2021-01-18 20:40:13 +0900

    add gugudan folder

---
+++ yejin/gugudan/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <gugudan />
+  </div>
+</template>
+
+<script>
+import app from './components/App.vue'
+
+export default {
+  name: 'App',
+  components: {
+    app
+  }
+}
+</script>
+
