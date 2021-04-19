bec24e67640d04c223a737d97043667a7804969b
HARDTACK-Dev <rjsdn827@gmail.com>
2021-04-20 01:10:01 +0900

    feat: CSS 작업

--- gunwoo/dashboard/src/views/addSchool/step1.vue
+++ gunwoo/dashboard/src/views/addSchool/step1.vue
@@ -13,11 +13,24 @@
         <!-- 전체 (1행 / 프리뷰) 나누기용 flex -->
         <div class="flex">
           <div class="row-1 flex">
-            section1
+            <!-- logo섹션 -->
             <div class="section-logo">
               <img :src="require('@/assets/' + logoImg)" alt="img-upload">
+              <p>학교 로고</p>
             </div>
             <div class="section-basic">
+              <!-- 학교이름 -->
+              <div class="name">
+                <p>학교명 | Name </p>
+                <input type="text">
+                <div class="verified"></div>
+              </div>
+              <!-- 주소 -->
+              <div class="address">
+                <p>주소 | Adress</p>
+                <input type="text" disabled>
+                <button>검색</button>
+              </div>
 
             </div>
           </div>
@@ -30,7 +43,17 @@
         <!-- 전체 flex 끝 -->
 
        <div class="row-2 flex">
-            section2
+            <div class="section-photos">
+              <!-- 사진 타이틀 -->
+              <div class="photo-title">
+                <p>사진 | {{PhotoUploaded}}개 업로드 됨</p>
+              </div>
+              <div class="photo-image">
+                <!-- <img :src="require('@/assets/' + photoImg)" alt="asdf"> -->
+              </div>
+
+
+            </div>
           </div>
 
         <div class="btn-section">
@@ -47,7 +70,12 @@
 export default {
     data(){
         return{
-            logoImg: 'upload-logo.png',
+          // 학교 로고 이미지 
+          logoImg: 'upload-logo.png',
+          // photoImg: 'upload-photo.png',
+
+          // 사진 업로드 개수
+          PhotoUploaded: 0
         }
     }    
 }
